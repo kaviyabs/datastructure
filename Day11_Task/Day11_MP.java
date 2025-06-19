c61055e20d8f518771d3550c46edb227d326f8d2
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-19 11:20:46 +0530

    latest commit

---
+++ Day11_Task/Day11_MP.java
@@ -0,0 +1,24 @@
+package day11;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;	
+import java.sql.Statement;
+
+	public class Day11_MP {
+		public static void main(String[] args) throws ClassNotFoundException, SQLException  {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			String url="jdbc:mysql://localhost:3306/test_db";
+			String username="root";
+			String password ="Kgisl@123";
+			Connection con = DriverManager.getConnection(url,username,password);
+			Statement stmt = con.createStatement();
+			String Query="select * from student";
+			ResultSet rs = stmt.executeQuery(Query);
+			
+			while(rs.next()) {
+				System.out.println(rs.getInt(1)+" "+rs.getString(2)+"  "+rs.getInt(3));
+			}
+		}
+	}
